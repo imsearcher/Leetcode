a727ae153676aea5b0db653a3acaad57be512045
Anonymous <imsearcher@gmail.com>
2015-11-06 00:47:06 +0000

    #70 Climbing Stairs

---
+++ ClimbingStairs.java
@@ -0,0 +1,27 @@
+public class ClimbingStairs {
+    public int climbStairs(int n) {
+   
+        if(n==0)
+            return 0;
+        
+        int[] result=new int[n+1];
+        for(int i=0;i<result.length;i++ )
+            result[i]=0;
+       
+        
+        return  fetch(n,result); 
+    }
+    public int fetch(int n, int[] result){
+        
+ 
+        if(n==1)
+            return 1;
+        if(n==2)
+            return 2;
+            
+        if(result[n]==0)
+            result[n]=fetch(n-1,result)+fetch(n-2,result);
+        return result[n];
+
+    }
+}

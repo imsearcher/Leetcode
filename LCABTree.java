e4a602be0264eaeb9348d8f2fe85c8c9ec934448
Anonymous <imsearcher@gmail.com>
2015-11-18 03:37:50 +0000

    #235 Lowest Common Ancestor of a Binary Search Tree #236 Lowest Common Ancestor of a Binary Tree

---
+++ LCABTree.java
@@ -0,0 +1,40 @@
+//#236 Lowest Common Ancestor of a Binary Tree
+//O(n)
+public class LCABTree {
+   public class TreeNode {
+     int val;
+     TreeNode left;
+     TreeNode right;
+     TreeNode(int x) { val = x; }
+    }
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        
+        return traversal(root,p,q);
+      
+        
+    }
+    public TreeNode traversal(TreeNode root, TreeNode p, TreeNode q){
+        
+          if(root==p||root==q){
+            return root;
+          }else{
+            if(root.left==null&&root.right==null)
+                return null;
+            TreeNode left=null;
+            TreeNode right=null;
+            if(root.left!=null)
+                left=traversal(root.left,p,q);
+            if(root.right!=null)
+                right=traversal(root.right,p,q);
+            if(left!=null&&right==null)
+                return left;
+            else if(left==null&&right!=null)
+                return right;
+            else if(left!=null&&right!=null)
+                return root;
+            else
+                return null;
+          }
+        
+    }
+}
